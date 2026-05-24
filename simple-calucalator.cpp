7cdd9172928667c8e9abea7b93647fe962bafda2
punyavathi-kopparthi <punyavathikopparthi9@gmail.com>
2026-05-24 09:56:39 +0530

    Add files via upload

---
+++ simple-calucalator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch(op) {
+
+        case '+':
+            cout << "Result = " << num1 + num2;
+            break;
+
+        case '-':
+            cout << "Result = " << num1 - num2;
+            break;
+
+        case '*':
+            cout << "Result = " << num1 * num2;
+            break;
+
+        case '/':
+            cout << "Result = " << num1 / num2;
+            break;
+
+        default:
+            cout << "Invalid Operator";
+    }
+
+    return 0;
+}
